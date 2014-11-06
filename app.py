6daec85ffa5a34bc7b95af0a6f8fbc02cddcaedb
nigelb <nigelbabu@gmail.com>
2014-11-06 21:25:59 +0530

    Add logging support

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
+import logging
 from os import environ
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+stream_handler = logging.StreamHandler()
+app.logger.setLevel(logging.DEBUG)
+app.logger.addHandler(stream_handler)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (environ.get('DATABASE_URL') or
                                          'sqlite:////tmp/test.db')
